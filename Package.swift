aa47f677077fd55e6eea1479c73364d09af5a1ed
Nike <nikechukwu@gmail.com>
2024-04-14 23:03:58 -0400

    Formatting

--- Package.swift
+++ Package.swift
@@ -4,30 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "Scarlet",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Scarlet",
-            targets: ["Scarlet"]
-        ),
-        .executable(
-            name: "ScarletCLI",
-            targets: ["ScarletCLI"]
-        )
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Scarlet"),
-        .target(
-            name: "ScarletCLI",
-            path: "CLI"
-        ),
-        .testTarget(
-            name: "ScarletTests",
-            dependencies: ["Scarlet"]
-        ),
-    ]
+  name: "Scarlet",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "Scarlet",
+      targets: ["Scarlet"]
+    ),
+    .executable(
+      name: "ScarletCLI",
+      targets: ["ScarletCLI"]
+    ),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "Scarlet"),
+    .target(
+      name: "ScarletCLI",
+      path: "CLI"
+    ),
+    .testTarget(
+      name: "ScarletTests",
+      dependencies: ["Scarlet"]
+    ),
+  ]
 )
